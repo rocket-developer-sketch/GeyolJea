9c518c84dc22b5a8defbb7f16988cd1dc8111086
nyk <drivingdeveloper@gmail.com>
2021-05-14 13:49:39 +0900

    create folder, DTO

---
+++ main/java/com/work/gyeoljea/DTO/UserDTO.java
@@ -0,0 +1,12 @@
+package com.work.gyeoljea.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDTO {
+	private String email;
+	private String password;
+	private String auth;
+}
