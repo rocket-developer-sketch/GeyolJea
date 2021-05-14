6c7439d381d8e891d30e3fb3a4f221b938affea2
nyk <drivingdeveloper@gmail.com>
2021-05-14 17:18:47 +0900

    comment spring security

--- main/java/com/work/gyeoljea/security/WebSecurityConfig.java
+++ main/java/com/work/gyeoljea/security/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.work.gyeoljea.security;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -24,13 +25,27 @@ import lombok.RequiredArgsConstructor;
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 	private final UserService userService;
-	
+
+	/*
+	 * configure(WebSecurty web)
+	 *	WebSecurity는filterChainProxy를 생성하는 필터입니다.
+ */
 	// 인증 무시할 경로 설정
+	
 	@Override
 	public void configure(WebSecurity web) {
-		web.ignoring().antMatchers("/css**", "/js/**", "/img");		
+		web.ignoring().antMatchers("/css/**", "/js/**", "/img/**", "favicon*");	
+		
+		/* static 자원 모두 제외 하는 법
+		 	web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations()); 
+	    */
 	}
 	
+	/*
+	 * configure(HttpSecurity http)
+	 *	HttpSecurity를 통해 HTTP요청에 대한 웹 기반의 보안을 구성할 수 있습니다.
+	 * 
+	 */
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		/* http 관련 인증
@@ -62,6 +77,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
 					.invalidateHttpSession(true) // 세션 제거
 		;
 	}
+	
+	
+	/*
+	 * configure(AuthenticationManagerBuilder auth)
+	 *	스프링시큐리티에서는 모든 인증은 AuthenticationManager를 통해 이루어지며 
+	 *  AuthenticationManager를 생성하기 위해서는 AuthenticationManagerBuilder를 사용합니다.
+	 *  즉, 인증을 위해서는 UserDetailService를 통해서 필요한 정보들을 가져옵니다.
+	 */
 	
 	// 로그인 할 때 필요한 정보 가져오기
 	@Override
